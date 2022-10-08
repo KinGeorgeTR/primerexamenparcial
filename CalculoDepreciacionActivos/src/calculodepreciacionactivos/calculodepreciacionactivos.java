56ed6f9f24692aae26d583ea64d39c6be3a489a2
KinGeorgeTR <slogan1301@gmail.com>
2022-10-07 18:00:07 -0600

    falta

---
+++ CalculoDepreciacionActivos/src/calculodepreciacionactivos/calculodepreciacionactivos.java
@@ -0,0 +1,29 @@
+package calculodepreciacionactivos;
+
+import java.util.Scanner;
+
+public class calculodepreciacionactivos {
+	
+
+		public static void main(String[] args) {
+			
+			
+			int valoresactivos,costoactivo, costo, Cant, Años, Depreciacion, Depreciacionanual, Costoactivo, Depreciacionacumulada;
+			
+			System.out.println("Ingresa el nombre del producto activo");
+			System.out.println("Ingresa los años de servicio del activo");
+			System.out.println("ingresa la cantidad de trabajo que realizo el activo");
+			System.out.println("Ingresa el costo del activo");
+			System.out.println("Ingresa el valor de la depreciacion porfavor");
+			
+			Costoactivo = costo;
+			
+			Depreciacionanual= Depreciacion;
+			
+			Depreciacionacumulada = Depreciacion * Años;
+			
+			valoresactivos
+			
+		}
+
+}
